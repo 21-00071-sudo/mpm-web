502251bb79a395cfcc3f10ccceb7f04f7d157594
21-00071-DTC-CFW <223986608+21-00071-sudo@users.noreply.github.com>
2025-08-12 11:43:14 +0800

    Initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MPM | DTC</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-center text-red-700 mt-56">Monitoring and Project Management Web App</h1>
+    <h2 class="text-center">Digital Transformation Center</h2>
+</body>
+</html>
