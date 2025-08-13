51ab54541bf09ab52264354eb024e82cfeebdc52
21-00071-DTC-CFW <223986608+21-00071-sudo@users.noreply.github.com>
2025-08-13 11:00:32 +0800

    feat: Add AuthController and login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
-    return view('auth.login');
-});
+Route::get('/', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth')->name('dashboard');
