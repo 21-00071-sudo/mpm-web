7ecf81038b31ca396a1b247bbce6eb62454c6b69
21-00071-DTC-CFW <223986608+21-00071-sudo@users.noreply.github.com>
2025-08-14 11:37:56 +0800

    refactor: Use flexbox for dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,9 +7,16 @@
 
      @vite('resources/css/app.css')
 </head>
-<body>
-    <x-header></x-header>
+<body class="flex min-h-screen">
+    
+    <div class="flex-1 flex flex-col">
+        <x-header></x-header>
 
-    <h1>Dashboard | {{ $user->role }}</h1>
+        <main class="m-6">
+            <h1 class="text-4xl font-bold">Hello, {{ $user->name }}!</h1>
+
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
