6541a3bd861dd376d2188a93678ad8b3a7b373cf
21-00071-DTC-CFW <223986608+21-00071-sudo@users.noreply.github.com>
2025-08-14 11:13:17 +0800

    feat: Create dashboard header partial

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<header class="bg-white p-6 flex flex-row items-center shadow-md">
+    <i class="fa-solid fa-bars fa-xl text-gray-700"></i>
+
+    <div class="font-bold text-xl pl-4 ">
+         <span class="text-red-500 font-semibold">DTC</span><span class="border-l border-gray-500 mx-1"></span>Monitoring and Project Management 
+    </div>
+
+    <div class="ml-auto flex">
+        <a href="#">
+            <i class="fa-solid fa-gear fa-lg text-gray-700"></i>
+        </a>
+        <div class="border-l border-gray-500 mx-4"></div>
+        <a href={{ route('logout') }}>
+            <i class="fa-solid fa-right-from-bracket fa-lg text-gray-700"></i>
+        </a>
+    </div>
+</header>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MPM | DTC</title>
+
+     @vite('resources/css/app.css')
 </head>
 <body>
+    <x-header></x-header>
+
     <h1>Dashboard | {{ $user->role }}</h1>
-    <a href={{ route('logout') }}>Logout</a>
 </body>
 </html>
