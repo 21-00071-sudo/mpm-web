aad9ce598e2c32b57feab8d65dd26b2a20517a8a
21-00071-DTC-CFW <223986608+21-00071-sudo@users.noreply.github.com>
2025-09-04 10:23:02 +0800

    feat: Add TaskUsers model, controller and migration

---
+++ app/Http/Controllers/TaskUserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskUserController extends Controller
+{
+    // Functions related to the TaskUser pivot table will be written here
+}
