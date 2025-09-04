ff513fd3e14ccc6c2ff025e337e4ced10e9bebdf
21-00071-DTC-CFW <223986608+21-00071-sudo@users.noreply.github.com>
2025-09-04 10:41:35 +0800

    feat: Add Folders model, controller, and migration

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FolderController extends Controller
+{
+    // Functions related to the Folder table will be written here
+}
